7e5fe2bb7dff3ddf53861369dfb27352387f79cd
jctarwater99 <jctarwater99@gmail.com>
2020-09-07 15:08:36 -0400

    Example for views

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    //<View style={styles.container}>
+    //  <Text>Open up App.js to start working on your app?</Text>
+    //  <StatusBar style="auto" />
+    //</View>
+    //  <View style={{ backgroundColor: "#0f0", flex: 1 }}></View>
+    //  <View style={{ backgroundColor: "#f00", flex: 1 }}></View>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#000",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "#f00",
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "#0f0",
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "#00f",
+          flex: 1,
+        }}
+      />
     </View>
   );
 }
@@ -14,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
