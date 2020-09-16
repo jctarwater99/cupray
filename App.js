3fa9b8514f9b45bfb67da7d227e7f543f4b7f44d
jctarwater99 <jctarwater99@gmail.com>
2020-09-16 16:47:52 -0400

    Testing database stuff

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Button, Text, View } from "react-native";
+import * as SQLite from "expo-sqlite";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     //<View style={styles.container}>
     //  <Text>Open up App.js to start working on your app?</Text>
@@ -10,6 +16,17 @@ export default function App() {
     //</View>
     //  <View style={{ backgroundColor: "#0f0", flex: 1 }}></View>
     //  <View style={{ backgroundColor: "#f00", flex: 1 }}></View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={testOne} />
+        <Stack.Screen name="SecondScreen" component={testTwo} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const testOne = ({ navigation }) => {
+  return (
     <View
       style={{
         flex: 1,
@@ -27,7 +44,12 @@ export default function App() {
           backgroundColor: "#0f0",
           flex: 1,
         }}
-      />
+      >
+        <Button
+          title="Press Me"
+          onPress={() => makeDatabase()} //navigation.navigate("SecondScreen")}
+        />
+      </View>
       <View
         style={{
           backgroundColor: "#00f",
@@ -36,7 +58,34 @@ export default function App() {
       />
     </View>
   );
+};
+
+function makeDatabase() {
+  const db = SQLite.openDatabase("db.cupray");
+  db.transaction((tx) => {
+    tx.executeSql(
+      "CREATE TABLE IF NOT EXISTS reqests (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT)"
+    );
+    tx.executeSql("INSERT INTO requests (description) values (?)", [
+      "gibberish",
+    ]);
+    tx.executeSql("SELECT * FROM requests", null, (tx, results) => {
+      let dataLen = results.rows.length;
+      alert(dataLen);
+    });
+  });
 }
+
+const testTwo = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#000",
+      }}
+    ></View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
