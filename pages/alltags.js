279962caa504f9f9aa51e643713676a136d64321
yiiradz <yiiradz@gmail.com>
2021-01-26 17:11:04 -0500

    Hamburger menu skeleton & additional pages

---
+++ pages/alltags.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Image, TouchableOpacity, Dimensions, FlatList } from "react-native";
+import { StyleSheet, Button, Text, View } from "react-native";
+import { populateDB } from "../database/populate";
+import { createDatabase, dropForTesting } from "../database/create";
+import * as queries from "../database/query";
+
+var { height, width } = Dimensions.get("window");
+
+
+const AllTags = ({ navigation }) => {
+    return ( 
+        <View style={styles.container}>
+            <Text style={[{ textAlign: 'center'}]}>All Tags</Text>
+        </View>
+    ) };
+
+    const styles = StyleSheet.create({
+        container: {
+            
+            marginTop: height * 0.2,
+            textAlign: "center",
+          },
+
+    });
+    export default AllTags;
